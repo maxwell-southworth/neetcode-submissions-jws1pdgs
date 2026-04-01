84d8793f1c54abac17a3eab500883bbbd082d505
Maxwell Southworth <MSSouthworth@nmhs.net>
2026-03-31 22:59:40 -0500

    Add: duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/duplicate-integer/submission-2.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public bool hasDuplicate(int[] nums) {
+        var hashSet = new HashSet<int>();
+        foreach (var num in nums)
+        {
+            if (hashSet.Contains(num))
+            {
+                return true;
+            }
+            hashSet.Add(num);
+        }
+        return false;
+    }
+}
